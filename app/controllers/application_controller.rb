37f320fb86a34e566be5b309a1ad64031e3e2f61
courtneyfeller <courtneyfeller@uchicago.edu>
2020-10-23 05:28:21 +0000

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,35 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
+
+   def homepage
+    render({ :template => "game_templates/rules.html.erb" })
+  end 
+ 
+  def play_rock
+
+     @comp_move = ["rock", "paper", "scissors"].sample
+
+          if @comp_move == "rock"
+            @outcome = "tied"
+      elsif @comp_move == "paper"
+            @outcome = "lost"
+      elsif @comp_move == "scissors" 
+            @outcome = "won"
+      end 
+
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end 
+
+
+  def play_paper
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end 
+
+  def play_scissors
+    render({ :template => "game_templates/user_scissors.html.erb" })
+  end 
 
 end
